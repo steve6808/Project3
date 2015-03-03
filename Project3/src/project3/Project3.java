fc0f2c31488d449b2f2b384ed5cd5cab70446102
Steven Benson <steve@10.142.205.190>
2015-03-03 18:18:45 -0500

    Project3

--- Project3/src/project3/Project3.java
+++ Project3/src/project3/Project3.java
@@ -10,14 +10,15 @@ package project3;
  * @author Joy P.
  * @author Steven B.
  */
- //Project 3 Search Engine UI
-
+// Project 3 Search Engine UI
+//
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
  
 public class Project3 {
+   
    private JFrame mainFrame;
    private JLabel headerLabel;
    private JLabel statusLabel;
@@ -28,23 +29,23 @@ public class Project3 {
    }
 
    public static void main(String[] args){
-      Project3  Project3 = new Project3();      
+      Project3  Project3 = new Project3();     
       Project3.showTextFieldDemo();
    }
 
    private void prepareGUI(){
       mainFrame = new JFrame("Project 3 Sarch Engine UI");
-      mainFrame.setSize(200,200);
+      mainFrame.setSize(400,400);
       mainFrame.setLayout(new GridLayout(3, 1));
       mainFrame.addWindowListener(new WindowAdapter() {
          public void windowClosing(WindowEvent windowEvent){
             System.exit(0);
-         }        
-      });    
-      headerLabel = new JLabel("Search Test", JLabel.CENTER);        
-      statusLabel = new JLabel("Search Results",JLabel.CENTER);    
+         }       
+      });   
+      headerLabel = new JLabel("Search Test", JLabel.CENTER);       
+      statusLabel = new JLabel("Search Results",JLabel.CENTER);   
 
-      statusLabel.setSize(120,75);
+      statusLabel.setSize(350,100);
 
       controlPanel = new JPanel();
       controlPanel.setLayout(new FlowLayout());
@@ -52,25 +53,29 @@ public class Project3 {
       mainFrame.add(headerLabel);
       mainFrame.add(controlPanel);
       mainFrame.add(statusLabel);
-      mainFrame.setVisible(true);  
+      mainFrame.setVisible(true); 
    }
 
    private void showTextFieldDemo(){
-      headerLabel.setText("Search Engine"); 
+      headerLabel.setText("Search Engine");
 
-      JLabel  searchLabel = new JLabel("search: ", JLabel.CENTER);
-      final JTextField userText = new JTextField(10);
-      final JTextField searchText = new JTextField(10);      
+      JLabel  searchLabel = new JLabel("Enter Search: ", JLabel.CENTER);
+      final JTextField userText = new JTextField(32);
+      final JTextField searchText = new JTextField(32);     
 
-      JButton searchButton = new JButton("Look up");
+      JButton searchButton = new JButton("Search");
       searchButton.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) { 
-            String data = "Your Answser: " + userText.getText();
+         public void actionPerformed(ActionEvent e) {
+     
+            String data = "Your Search Results: " + userText.getText();
+            data += new String(searchText.getText());
+            statusLabel.setText(data);       
          }
-      }); 
-      controlPanel.add(searchLabel);       
+      });
+
+      controlPanel.add(searchLabel);      
       controlPanel.add(searchText);
       controlPanel.add(searchButton);
-      mainFrame.setVisible(true);  
+      mainFrame.setVisible(true); 
    }
 }
